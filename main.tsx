c2cbf18b921dc83bb0b686f31022459a54bb5098
AbinashRai <abinashbantawa1905@gmail.com>
2026-03-09 02:34:11 +0545

    added instructions for adding bms to homescreen

--- main.tsx
+++ main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import "@khmyznikov/pwa-install";
 import App from "@/app/App";
 import "@/app/globals.css";
 
